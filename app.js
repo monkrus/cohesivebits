a0c3d47d278032e7be728965958bdb91f5fa05dd
Arpad Asztalos <arpad.web@gmail.com>
2017-04-09 07:36:36 -0700

    added node_modules for heroku

---
+++ app.js
@@ -0,0 +1,9 @@
+var static = require('node-static');
+
+var fileServer = new static.Server('./src');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}).listen(process.env.PORT || 3000);
